c70ee6928daa0080fb05648c713c63629fd8d3c6
lwenke01 <lwenke@gmail.com>
2017-04-12 20:38:24 -0700

    added more companies

--- app/bots/leadership.js
+++ app/bots/leadership.js
@@ -1,41 +1,52 @@
 'use strict';
 
-const request = require('request');
-const cheerio = require('cheerio');
-const fs = require('fs');
-
-//blog lists
-var blog1 = {url:"http://www.merck.com/about/leadership/executive-committee/home.html", name: "Merck", category: "contact"};
-var blog2 = {url:"https://davidwalsh.name", name: "David Walsh", category: "contact"};
+// var request = require('request');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
 
 
-//codinghorror
-request(blog1.url, function(error, response, body) {
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+var _id,company,name, title, yearStart,image,description,link, source, lastUpdated;
+var json = {_id: '', company: '',name: '',title: '', yearStart:'', image: '',description:'',  link:'', source:'', lastUpdated:''};
+
+
+//company lists
+var company1 = {url:'http://www.merck.com/about/leadership/executive-committee/home.html', name: 'Merck', root:'http://www.merck.com', category: 'contact'};
+var company2 = {url:'http://www.pfizer.com/about/leadership-and-structure/meet-executive-leaders', name: 'pfizer', root: 'http://www.pfizer.com',category: 'contact'};
+var company3 = {url:'http://www.gsk.com/en-gb/about-us/corporate-executive-team/', name: 'GSK', root: 'http://www.gsk.com,category'};
+
+
+
+// company1
+request(company1.url, function(error, response, body) {
   if(error) {
-    console.log("Error: " + error);
+    console.log('Error: ' + error);
   }
-  console.log("Status code: " + response.statusCode);
+  console.log('Status code: ' + response.statusCode + ': ' + company1.name);
 
   var $ = cheerio.load(body);
 
-  var name, title, yearStart,image,description,link, source, lastUpdated;
-  var json = {name: "",title: "", yearStart:"", image: "",description:"",  link:"", source:"", lastUpdated:""};
 
 
   $('div.content_container > div.container > .row >.column_middle >.row').each(function( index ){
 
-var sumArray = [];
-$(this).find('div.col-lg-9.col-md-9.col-sm-12 > ul > li').each(function( index){
-  var li_text = $(this).text().trim();
-  sumArray.push(li_text);
-});
+    var sumArray = [];
+    $(this).find('div.col-lg-9.col-md-9.col-sm-12 > ul > li').each(function( index){
+      var li_text = $(this).text().trim();
+      sumArray.push(li_text);
+    });
     var name =  $(this).find('div.col-lg-3.col-md-3.col-sm-12 > img').attr('alt');
+
     var title = sumArray[0]
-    var image = "http://www.merck.com" + $(this).find('div.col-lg-3.col-md-3.col-sm-12 > img').attr('src');
+    var image = $(this).find('div.col-lg-3.col-md-3.col-sm-12 > img').attr('src');
     var description =sumArray[0];
     var link = $(this).find('div.col-lg-9.col-md-9.col-sm-12 > a').attr('href');
 
-    var source = blog1.url;
+    var source = company1.url;
+    var un = image.split('/');
     var lastUpdated = new Date();
     var titleArray = [];
     var n =title.split(',');
@@ -50,70 +61,118 @@ $(this).find('div.col-lg-9.col-md-9.col-sm-12 > ul > li').each(function( index){
     var nlen = n.length;
 
     n.forEach(function(v, i){
-        if(v !== ' Merck & Co.' || ' Inc.'){
+      if(v !== ' Merck & Co.' || ' Inc.'){
 
         titleArray.push(v)
-        }
-      })
+      }
+    })
+    json._id = un[un.length -1].split('.')[0].toLowerCase();
+        json.company = company1.name;
+    json.name = name.toProperCase();
+    if(titleArray.length === 0){
+      json.title = title.toProperCase();
+    } else {
+      json.title = titleArray.join(',').toProperCase();
 
-      // console.log(i);
-      // if(v !=='Inc.' || 'Merck & Co.' ){
-      //       titleArray.push(v);
-      //
-      //     }
+    }
+    json.yearStart = yearStart.split(' ').join('');
+    json.image = 'http://www.merck.com' + image;
+    json.description = sumArray;
+    json.link = link;
+    json.source = source
+    json.lastUpdated = lastUpdated;
+    //print to json
 
-
-
-    // var nLink = link.split('/');
-    // var newLink = nLink.pop().split(' ').join('%20');
-//save as JSON data
-  json.name = name;
-if(titleArray.length === 0){
-  json.title = title;
-} else {
-  json.title = titleArray.join(',');
-
-}
-json.yearStart = yearStart.split(' ').join('');
-      json.image = image;
-      json.description = sumArray;
-      json.link = link;
-
-      json.source = source
-      json.lastUpdated = lastUpdated;
-  //print to json
-      // $('#stories').appendTo(json);
-       fs.appendFileSync('app/output/leadership.json', JSON.stringify(json) + '\n');
+    fs.appendFileSync('app/output/leadership.json', JSON.stringify(json) + '\n');
 
   });
 });
 
-//davidwalsh
-// request(blog2.url, function(error, response, body) {
-//   if(error) {
-//     console.log("Error: " + error);
-//   }
-//   console.log("Status code: " + response.statusCode);
-//
-//   var $ = cheerio.load(body);
-//
-//   var title, description, date, link, source;
-//   var json = {title: "", description:"", date:"", link:"", source:""};
-//
-//   $('#main > ul.post-list > li').each(function( index ) {
-//     var title = $(this).find('span > img').attr('alt');
-//     var description = $(this).find('div.preview > p').text().trim().slice(0,100);
-//     var date = $(this).find('div.meta > time').text().trim();
-//     var link = $(this).attr('data-url');
-//     var source = blog2.name;
-//
-// //save as JSON data
-//       json.title = title;
-//       json.description = description;
-//       json.date = date;
-//       json.link = link;
-//       json.source = source;
-//   //print to json
-//        fs.appendFileSync('app/output/tech.json', JSON.stringify(json) + '\n');
-//   });
-// });
+//company2
+request(company2.url, function(error, response, body) {
+  if(error) {
+    console.log('Error: ' + error);
+  }
+  console.log('Status code: ' + response.statusCode);
+  var $ = cheerio.load(body);
+
+  $('.profile-media-block-container2 > .profile-media-block2.executive-block').each(function( index ){
+
+    var _id = $(this).find('img').attr('alt').split('.')[0];
+    var name =  $(this).find('h2.profile-media-block-title').text().trim().split(',')[0];
+    var title =  $(this).find('span.profile-media-block-subtitle').text().trim();
+    var title2 = $(this).text().trim().split('\r\n\t\t')[1];
+
+    var image = $(this).find('img').attr('src');
+    var description ='hey';
+    var link = $(this).find('a.profile-media-block-link').attr('href');
+    var source = company2.url;
+    var lastUpdated = new Date();
+
+    json._id = _id;
+        json.company = company2.name;
+    json.name = name.toProperCase();
+    if(!title) {
+      json.title = title2.toProperCase();
+    } else {
+      json.title = title.toProperCase();
+    }
+
+    json.yearStart = '';
+    json.image =  company2.root + image;
+    json.description = description;
+    json.link = company2.root + link;
+
+    json.source = source;
+    json.lastUpdated = lastUpdated;
+    //print to json
+
+    fs.appendFileSync('app/output/leadership.json', JSON.stringify(json) + '\n');
+
+  });
+});
+
+request(company3.url, function(error, response, body) {
+  if(error) {
+    console.log('Error: ' + error);
+  }
+  console.log('Status code: ' + response.statusCode);
+
+  var $ = cheerio.load(body);
+  $('.main-container > ul.grid-listing__list > li.grid-listing__item').each(function( index ){
+
+    var name =  $(this).find('.grid-listing__content > h2').text().trim();
+    var title =  $(this).find('.grid-listing__content > p').text().trim();
+    var title2 = $(this).text().trim().split('\r\n\t\t')[1];
+
+    var image = $(this).find(' a > div.grid-listing__img > img').attr('data-src');
+
+    var description ='hey';
+    var link = $(this).find('a').attr('href');
+    var source = company3.url;
+    var lastUpdated = new Date();
+    var id_spl = link.split('/');
+    var _id = id_spl[id_spl.length -2];
+
+    json._id = _id;
+    json.company = company3.name;
+    json.name = name.toProperCase();
+    if(!title) {
+      json.title = title2.toProperCase();
+    } else {
+      json.title = title.toProperCase();
+    }
+
+    json.yearStart = '';
+    json.image = company3.root +  image;
+    json.description = description;
+    json.link = company3.root + link;
+
+    json.source = source;
+    json.lastUpdated = lastUpdated;
+    //print to json
+
+    fs.appendFileSync('app/output/leadership.json', JSON.stringify(json) + '\n');
+
+  });
+});
